741126efd47724951577f936a765c5d79508a73a
ThaiSonP <tysonpan@pursuit.org>
2019-07-10 20:54:59 -0400

    redesign origins and skills

--- backend/data/tft.sql
+++ backend/data/tft.sql
@@ -17,7 +17,12 @@ CREATE TABLE class_skills(
 
 CREATE TABLE origin(
   id SERIAL PRIMARY KEY,
-  origin VARCHAR,
+  origin VARCHAR
+);
+
+CREATE TABLE origin_skills(
+  id SERIAL PRIMARY KEY,
+  origin_id INT REFERENCES origin(id),
   requirement INT,
   output VARCHAR
 );
@@ -25,17 +30,29 @@ CREATE TABLE origin(
 CREATE TABLE heroes (
   id SERIAL PRIMARY KEY,
   champion_name VARCHAR ,
-  class_id INT references class(id) ,
   origin INT REFERENCES origin(id),
-  health INT,
-  armor INT,
-  magic_resistance INT,
-  physical_damage INT,
-  attack_speed INT,
-  range INT,
+  class_id INT references class(id) ,
+  class_id_2 INT references class(id) ,
   tier INT,
-  level INT,
   pool_count INT
+);
+
+CREATE TABLE heroes_stats (
+  id SERIAL PRIMARY KEY,
+  hero_id INT REFERENCES heroes(id) ,
+  cost INT,
+  health INT,
+  mana INT,
+  DPS int,
+  physical_damage INT,
+  critical_chance INT,
+  attack_speed INT,
+  attack_range INT,
+  magic_resistance INT,
+  armor INT,
+  level INT,
+  special VARCHAR,
+  special_damage INT
 );
 
 INSERT INTO class(class) VALUES
@@ -70,29 +87,51 @@ INSERT INTO class_skills(class_id, requirement, output) VALUES
 (10,3,'Sorcerers gain double mana from attacking and allies have increased +35% spell damage'),
 (10,6,'Sorcerers gain double mana from attacking and allies have increased +100% spell damage');
 
-INSERT INTO origin (origin,requirement,output) VALUES
-('Demon',2,'Attacks from Demons have a 40% chance to burn all of an enemys mana and deal that much true damage'),
-('Demon',4,'Attacks from Demons have a 60% chance to burn all of an enemys mana and deal that much true damage.'),
-('Demon',6,'Attacks from Demons have a 80% chance to burn all of an enemys mana and deal that much true damage.'),
-('Dragon',2,'Dragons are immune to Magic damage'),
-('Exile',1,'If an Exile has no adjacent allies at the start of combat, they gain a shield equal to a 100% max health.'),
-('Glacial',2,'On hit, Glacials have a 20% chance to stun for 2s.'),
-('Glacial',4,'On hit, Glacials have a 30% chance to stun for 2s.'),
-('Glacial',6,'On hit, Glacials have a 45% chance to stun for 2s.'),
-('Imperial',2,'1 random Imperial deals Double Damage.'),
-('Imperial',4,'All Imperials deal Double Damage.'),
-('Noble',3,'Nobles grant +100 Armor and heal for 35 on hit to 1 random ally'),
-('Noble',6,'Nobles grant +100 Armor and heal for 35 on hit to all allies'),
-('Ninja',1,'1 Ninja only: Ninja gains +40% Attack Damage'),
-('Ninja',4,'4 Ninjas: All Ninjas gain +60% Attack Damage'),
-('Pirate',3,'Earn up to 4 additional gold after combat against another player.'),
-('Phantom',2,'Curse a random enemy at the start of combat, setting their HP to 100.'),
-('Robot',1,'Robots start combat at full mana'),
-('Void',3,'Your teams basic attacks ignore 50% of the enemys Armor'),
-('Wild',2,'Attacks generate stacks of Fury (up to 5). Each stack of Fury grants 7% Attack Speed - Wild allies only'),
-('Wild',4,'Attacks generate stacks of Fury (up to 5). Each stack of Fury grants 7% Attack Speed - All allies'),
-('Yordle',3,'Attacks against ally Yordles have a 25% chance to miss.'),
-('Yordle',6,'Attacks against ally Yordles have a 60% chance to miss.');
+INSERT INTO origin (origin) VALUES
+('Demon'),
+('Dragon'),
+('Exile'),
+('Glacial'),
+('Imperial'),
+('Noble'),
+('Ninja'),
+('Pirate'),
+('Phantom'),
+('Robot'),
+('Void'),
+('Wild'),
+('Yordle');
 
--- INSERT INTO  heroes (champion_name, class_id, origin, health, armor, magic_resistance, physical_damage, attack_speed, range, tier, level, pool_count) VALUES
--- ('Aatrox',)
+INSERT INTO origin_skills(origin_id, requirement, output) VALUES
+(1,2,'Attacks from Demons have a 40% chance to burn all of an enemys mana and deal that much true damage'),
+(1,4,'Attacks from Demons have a 60% chance to burn all of an enemys mana and deal that much true damage.'),
+(1,6,'Attacks from Demons have a 80% chance to burn all of an enemys mana and deal that much true damage.'),
+(2,2,'Dragons are immune to Magic damage'),
+(3,1,'If an Exile has no adjacent allies at the start of combat, they gain a shield equal to a 100% max health.'),
+(4,2,'On hit, Glacials have a 20% chance to stun for 2s.'),
+(4,4,'On hit, Glacials have a 30% chance to stun for 2s.'),
+(4,6,'On hit, Glacials have a 45% chance to stun for 2s.'),
+(5,2,'1 random Imperial deals Double Damage.'),
+(5,4,'All Imperials deal Double Damage.'),
+(6,3,'Nobles grant +100 Armor and heal for 35 on hit to 1 random ally'),
+(6,6,'Nobles grant +100 Armor and heal for 35 on hit to all allies'),
+(7,1,'1 Ninja only: Ninja gains +40% Attack Damage'),
+(7,4,'4 Ninjas: All Ninjas gain +60% Attack Damage'),
+(8,3,'Earn up to 4 additional gold after combat against another player.'),
+(9,2,'Curse a random enemy at the start of combat, setting their HP to 100.'),
+(10,1,'Robots start combat at full mana'),
+(11,3,'Your teams basic attacks ignore 50% of the enemys Armor'),
+(12,2,'Attacks generate stacks of Fury (up to 5). Each stack of Fury grants 7% Attack Speed - Wild allies only'),
+(12,4,'Attacks generate stacks of Fury (up to 5). Each stack of Fury grants 7% Attack Speed - All allies'),
+(13,3,'Attacks against ally Yordles have a 25% chance to miss.'),
+(13,6,'Attacks against ally Yordles have a 60% chance to miss.');
+
+INSERT INTO heroes(champion_name,origin,class_id ,class_id_2 ,tier,pool_count) VALUES
+('Aatrox',1,2,null,3,21),
+('Ahri',12,10,null,2,26),
+('Akali',7,1,null,4,13),
+('Anivia',4,4,null,5,10),
+('Ashe',4,8,null,3,21),
+('Aurelion-Sol',2,10,null,4,13),
+('Blitzcrank',10,3,null,2,26),
+('Brand',1,4,null,4,13),;
