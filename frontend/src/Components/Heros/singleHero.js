0e9efb9f9c790fc712f6958d1fa2836b006897ab
ThaiSonP <tysonpan@pursuit.org>
2019-09-20 21:18:52 -0400

    organize table

--- frontend/src/Components/Heros/singleHero.js
+++ frontend/src/Components/Heros/singleHero.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 import axios from 'axios'
 import index from '../../Images/ChampionIcons/index.js'
-import Stats from './stats.js'
+// import Stats from './stats.js'
 
 
 class SingleHero extends Component {
@@ -29,34 +29,77 @@ class SingleHero extends Component {
       })
     })
   }
+  renderTable =()=>{
+    if(this.state.stats){
+      return this.state.stats.map((stats,i)=>{
+        const{armor,attack_speed,critical_chance,dps,health,level,magic_resistance,mana,physical_damage,special,special_damage,special_health}=stats
+        return(
+          <tr key={i}>
+            <td>{level}</td>
+            <td>{health}</td>
+            <td>{mana}</td>
+            <td>{physical_damage} </td>
+            <td>{attack_speed}</td>
+            <td>{dps}</td>
+            <td>{critical_chance}%</td>
+            <td>{special}</td>
+            <td>{special_damage}</td>
+            <td>{special_health}</td>
+            <td>{armor}</td>
+            <td>{magic_resistance}</td>
+          </tr>
+        )
+      })}
+ }
+
   componentDidMount(){
     this.getData(this.state.heroId)
   }
 
   render(){
-    const {name,class_id,className,origin_id,originName,stats}=this.state
-
+    const {name,class_id,className,origin_id,originName}=this.state
     return(
       <div>
         <div>
           <img src= {index[name]} alt =''/>
         </div>
         <div>
-          Champion: {name}
+          Champion : {name}
         </div>
         <div>
-          Class 1:
+          Class :
             <Link to={`/class/${ class_id}` }>
               { className}
             </Link>
         </div>
         <div>
-          Origin 1:
+          Origin :
             <Link to={`/origins/${ origin_id}` }>
               { originName}
             </Link>
         </div>
-        <Stats stats={stats}/>
+        <table>
+          <tbody>
+            <tr>
+              <th>Level</th>
+              <th>Max Health</th>
+              <th>Max Mana</th>
+              <th>Physical Damage </th>
+              <th>Attack Speed</th>
+              <th>Damage Per Second</th>
+              <th>Critical Chance</th>
+              <th>Special</th>
+              <th>Special Damage</th>
+              <th>Special Health</th>
+              <th>Armor</th>
+              <th>Magic Resistance</th>
+            </tr>
+            {this.renderTable()}
+          </tbody>
+
+        </table>
+
+
       </div>
     )
   }
--- frontend/src/Components/Heros/stats.js
+++ frontend/src/Components/Heros/stats.js
@@ -1,19 +1,40 @@
 import React, {Component} from 'react'
 
-class Stats extends Component {
-  constructor(props){
-    super(props)
-    this.state = ({
-      stats:this.props.stats
+const Stats = props =>{
+   const renderTable =()=>{
+    return props.stats((stat,i)=>{
+      const{armor,attack_speed,critical_chance,dps,health,level,magic_resistance,mana,physical_damage,special,special_damage,special_health}=props.stats
+      return(
+        <tr key={i}>
+          <td>{level}</td>
+          <td>{health}</td>
+          <td>{mana}</td>
+          <td>{physical_damage} </td>
+          <td>{attack_speed}</td>
+          <td>{dps}</td>
+          <td>{critical_chance}</td>
+          <td>{special}</td>
+          <td>{special_damage}</td>
+          <td>{special_health}</td>
+          <td>{armor}</td>
+          <td>{magic_resistance}</td>
+        </tr>
+      )
     })
   }
-  render(){
-    // console.log(this.props)
-    console.log(this.state)
+
+  if(!props){
+    return (
+      <div>
+        Loading!
+      </div>
+    )
+  }else{
+    // console.log(props.stats)
     return(
       <div>
-
-      this is stats
+        // {this.renderTable()}
+        this is stats
       </div>
     )
   }
